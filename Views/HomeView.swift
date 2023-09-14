b9d4fe80c65ca48f2513792bc2ddb8f50e1dec2d
Sumit <sumitdhungana14@gmail.com>
2023-09-14 13:36:39 -0500

    Run background task every 5 seconds.

--- Views/HomeView.swift
+++ Views/HomeView.swift
@@ -12,6 +12,8 @@ struct homeView: View {
     let tokenKey = "token"
     
     @State private var logout: Bool = false
+    @State private var backgroundTaskID: UIBackgroundTaskIdentifier = .invalid
+    @ObservedObject var locationManager = LocationManager()
 
     var body: some View {
         NavigationView {
@@ -37,6 +39,51 @@ struct homeView: View {
                         }
         }
         .navigationBarBackButtonHidden(true)
+        .onAppear {
+            registerBackgroundTask()
+                
+            startPeriodicBackgroundTask()
+        }
+    }
+    
+    private func registerBackgroundTask() {
+        backgroundTaskID = UIApplication.shared.beginBackgroundTask()
+        print("Background task registered successfully: \(backgroundTaskID)")
+
+    }
+
+    private func endBackgroundTask() {
+        UIApplication.shared.endBackgroundTask(backgroundTaskID)
+        backgroundTaskID = .invalid
+    }
+
+    private func performBackgroundTask() {
+        let timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+                   
+            if let location = locationManager.location {
+                print("Latitude: \(location.coordinate.latitude)")
+                print("Longitude: \(location.coordinate.longitude)")
+                
+                //Call api to post location data
+                //Depending on api response, generate hourly notification
+                //If api returns unauthorized, logout and end background task, the user has to login again
+            }
+                   
+//            let errorOccurred = false
+//            if errorOccurred {
+//                self.endBackgroundTask()
+//            }
+        }
+               
+        // Ensure the timer is properly invalidated when the task is done
+        RunLoop.current.add(timer, forMode: .default)
+        RunLoop.current.run()
+    }
+    
+    private func startPeriodicBackgroundTask() {
+        DispatchQueue.global().async {
+            self.performBackgroundTask()
+        }
     }
 }
 
